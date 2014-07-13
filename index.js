42a062b504e7a2f5e8b45ce423b4a5073929b0b2
Ben Kus <ben.kus@gmail.com>
2014-07-12 23:52:28 -0700

    Updated to try to get the higher-res apple favicons first, then other specified icons, then the default favicon

--- index.js
+++ index.js
@@ -18,31 +18,50 @@ var request = require('request')
 module.exports = function(url, callback) {
   var p    = Url.parse(url)
     , root = p.protocol + "//" + p.host
-    , ico  = root + "/favicon.ico";
+    , defaultIco  = root + "/favicon.ico";
   
   // Check the root of the web site.
-  does_it_render(ico, function(err, renders) {
+  does_it_render(defaultIco, function(err, renders) {
+    var ico;
     if (err) return callback(err);
-    if (renders) return callback(null, ico);
     
     // Check for <link rel="icon" href="???"> tags to indicate
     // the location of the favicon.
-    request(root, function(err, res, body) {
+    function checkLinks(rel_re, body) {
       var link_re = /<link (.*)>/gi
-        , rel_re  = /rel=["'][^"]*icon[^"']*["']/i
         , href_re = /href=["']([^"']*)["']/i
         , match, ico_match;
-      
+
       while (match = link_re.exec(body)) {
         if (rel_re.test(match[1]) && (ico_match = href_re.exec(match[1]))) {
           ico = ico_match[1];
-          if (ico[0] == "/") {
+          if (ico.substring(0, 2) == "//") {
+            ico = p.protocol + ico;
+          } else if (ico[0] == "/") {
             ico = root + ico;
           }
-          return callback(null, ico);
+          return ico;
         }
       }
-      
+      return false;
+    }
+
+    request(root, function(err, res, body) {
+      // var icons = [];
+      var apple_rel_re  = /rel=["'][^"]*apple-touch-icon[^"']*["']/i;
+      var rel_re  = /rel=["'][^"]*icon[^"']*["']/i;
+      var ico;
+      // check for apple icons first
+      ico = checkLinks(apple_rel_re, body);
+      if (ico) return callback(null, ico);
+
+      // then check for regular icons
+      ico = checkLinks(rel_re, body);
+      if (ico) return callback(null, ico);
+
+      // then check for /favicon.ico
+      if (renders) return callback(null, defaultIco);
+
       // No favicon could be found.
       return callback(null, null);
     });
