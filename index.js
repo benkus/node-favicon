48de6ee3806c21cf4d90604a7367c793d9b1542b
Ben Kus <ben.kus@gmail.com>
2014-07-14 00:38:27 -0700

    Removed some legacy unused code

--- index.js
+++ index.js
@@ -31,112 +31,105 @@ function getFavicons(url, callback) {
   }
 
   var p    = Url.parse(url)
-    , root = p.protocol + "//" + p.host
-    , defaultIco  = root + "/favicon.ico";
+    , root = p.protocol + "//" + p.host;
   
-  // Check the root of the web site.
-  does_it_render(defaultIco, function(err, renders) {
-    var ico;
-    if (err) return callback(err);
-    
-    // Check for <link rel="icon" href="???"> tags to indicate
-    // the location of the favicon.
-    function checkLinks(rel_re, body) {
-      var link_re = /<link (.*)>/gi
-        , href_re = /href=["']([^"']*)["']/i
-        , match, ico_match;
-      var icons = [];
+  // Check for <link rel="icon" href="???"> tags to indicate
+  // the location of the favicon.
+  function checkLinks(rel_re, body) {
+    var link_re = /<link (.*)>/gi
+      , href_re = /href=["']([^"']*)["']/i
+      , match, ico_match;
+    var icons = [];
 
-      while (match = link_re.exec(body)) {
-        if (rel_re.test(match[1]) && (ico_match = href_re.exec(match[1]))) {
-          ico = ico_match[1];
-          if (ico.substring(0, 2) == "//") {
-            ico = p.protocol + ico;
-          } else if (ico[0] == "/") {
-            ico = root + ico;
-          } else if (ico.substring(0, 4) != 'http') {
-            ico = root + "/" + ico;
-          }
-          icons.push(ico);
+    while (match = link_re.exec(body)) {
+      if (rel_re.test(match[1]) && (ico_match = href_re.exec(match[1]))) {
+        ico = ico_match[1];
+        if (ico.substring(0, 2) == "//") {
+          ico = p.protocol + ico;
+        } else if (ico[0] == "/") {
+          ico = root + ico;
+        } else if (ico.substring(0, 4) != 'http') {
+          ico = root + "/" + ico;
         }
+        icons.push(ico);
       }
-      if (icons.length == 0) {
-        return false;  
-      } else {
-        return icons;
-      }
-      
+    }
+    if (icons.length == 0) {
+      return false;  
+    } else {
+      return icons;
+    }
+    
+  }
+
+  request(root, function(err, res, body) {    
+    if (err) {
+      return callback(err);
+    }
+    var ico;
+    var icons = [];
+
+    // include apple icons first
+    var apple_rel_re  = /rel=["'][^"]*apple-touch-icon[^"']*["']/i;
+    var apple_icons = checkLinks(apple_rel_re, body);
+    if (apple_icons) 
+      icons = icons.concat(apple_icons);
+
+    // then regular icons
+    var rel_re  = /rel=["'][^"]*icon[^"']*["']/i;
+    var other_icons = checkLinks(rel_re, body);
+    if (other_icons) 
+      icons = icons.concat(other_icons);
+
+    // then check for /favicon.ico
+    var defaultIco  = root + "/favicon.ico";
+    icons.push(defaultIco);
+
+    if (icons.length == 0) {
+      // No favicon could be found.
+      return callback(null, null);
     }
 
-    request(root, function(err, res, body) {
-      var apple_rel_re  = /rel=["'][^"]*apple-touch-icon[^"']*["']/i;
-      var rel_re  = /rel=["'][^"]*icon[^"']*["']/i;
-      var ico;
-      var icons = [];
+    // remove duplicate icons
+    var urls = icons.filter(function(elem, pos) {
+      return icons.indexOf(elem) == pos;
+    }); 
 
-      // check for apple icons first
-      var apple_icons = checkLinks(apple_rel_re, body);
-      if (apple_icons) 
-        icons = icons.concat(apple_icons);
+    // get info for each image
+    var faviconsInfo = [];
+    var responses = [];
+    var completed_requests = 0;
+    for (var i=0; i<urls.length; i++) {
+      request({ url: urls[i], 
+                encoding: null,
+                timeout: 5000, 
+              }, function(err, res, body) {
+        if (res && body) {
+          responses.push({
+            url: res.request.uri.href, 
+            err: err, 
+            res: res, 
+            body: body
+          });
+        }
+        completed_requests++;
 
-      // then check for regular icons
-      var other_icons = checkLinks(rel_re, body);
-      if (other_icons) 
-        icons = icons.concat(other_icons);
-
-      // then check for /favicon.ico
-      if (renders) {
-        icons.push(defaultIco);
-      }
-
-      if (icons.length == 0) {
-        // No favicon could be found.
-        return callback(null, null);
-      }
-
-      // remove duplicate icons
-      var urls = icons.filter(function(elem, pos) {
-        return icons.indexOf(elem) == pos;
-      }); 
-
-      // get info for each image
-      var faviconsInfo = [];
-      var responses = [];
-      var completed_requests = 0;
-      for (var i=0; i<urls.length; i++) {
-        request({ url: urls[i], 
-                  encoding: null,
-                  timeout: 5000, 
-                }, function(err, res, body) {
-          if (res && body) {
-            responses.push({
-              url: res.request.uri.href, 
-              err: err, 
-              res: res, 
-              body: body
-            });
-          }
-          completed_requests++;
-
-          if (completed_requests == urls.length) { 
-            // got all the images, now process them
-            for (var j=0; j<responses.length; j++) {
-              if (responses[j].res.statusCode == 200) { // ignore broken favicon urls
-                var imageData = extractImageData(responses[j]);
-                if (imageData.format !== '' ) { // ignore formats we don't recognize
-                  faviconsInfo.push(imageData);
-                }
+        if (completed_requests == urls.length) { 
+          // got all the images, now process them
+          for (var j=0; j<responses.length; j++) {
+            if (responses[j].res.statusCode == 200) { // ignore broken favicon urls
+              var imageData = extractImageData(responses[j]);
+              if (imageData.format !== '' ) { // ignore formats we don't recognize
+                faviconsInfo.push(imageData);
               }
             }
-            // sort by size
-            faviconsInfo = faviconsInfo.sort(compare); 
-            return callback(null, faviconsInfo);
           }
-        });
-      }
-
-      
-    });
+          // sort by size
+          faviconsInfo = faviconsInfo.sort(compare); 
+          return callback(null, faviconsInfo);
+        }
+      });
+    }
   });
 };
 
@@ -165,16 +158,6 @@ function extractImageData(favIconResponse) {
     imageData.height = info.height;
   }
   return imageData;
-}
-
-// Internal: Check the status code.
-function does_it_render(url, callback) {
-  request({ url: url, 
-            timeout: 5000,
-          }, function(err, res, body) {
-    if (err) return callback(err);
-    callback(null, res.statusCode == 200);
-  });
 }
 
 function compare(a,b) {
